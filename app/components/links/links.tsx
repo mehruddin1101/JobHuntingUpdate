263e7a0b6322ec000a45fa99dc7799dc2470fdd2
mehruddin11 <mehruddin881@gmail.com>
2024-04-11 23:07:37 +0530

    changes

--- app/components/links/links.tsx
+++ app/components/links/links.tsx
@@ -1,14 +1,11 @@
-"use client";
-
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import DevelopNators from "../../../public/developnators1.png"; // Correct image import path
 
-interface Link {}
+
 
 const Links = () => {
   const [links, setLinks] = useState([]);
-
-
 
   const getAllLinks = async () => {
     try {
@@ -16,7 +13,7 @@ const Links = () => {
         "https://developnators.azurewebsites.net/api/JobHunting/GetAllJobs"
       );
       const response = await data.json();
-      console.log(response)
+      console.log(response);
       setLinks(response);
     } catch (error) {
       console.error("Error fetching links:", error);
@@ -28,26 +25,40 @@ const Links = () => {
   }, []);
 
   return (
-    <div  className="mt-10">
+    <div className="mt-10">
       <div className="links-container">
         <div className="card-data"></div>
 
-        <div className="card-data-10"></div>
+        <div className="card-data-10" style={{ backgroundColor: 'black', color: 'white', textAlign: 'center' }}>
+          <div style={{padding:"5px"}}>
+         <h1 style={{fontWeight:'400'}}>Our Training Program</h1>
+         <hr/>
+
+          </div>
+          <a target="_blank" href = "https://developnator.web.app/" >
+  <img
+    style={{ objectFit: 'contain', width: '100%', height: '100%', display: 'block', margin: '0 auto' }}
+    src={DevelopNators.src}
+    alt="DevelopNators"
+  />
+   </a>
+</div>
+
+
 
         <div className="card-data-20"></div>
 
-        <div className="links-wrapper card-data-links">
+        <div className="links-wrapper card-data-links " >
           {links.map((res: any, index) => {
             return (
-              <div style={{ display: "flex",}} key={index}>
-                <a style={{padding:'2rem'}} className="mt-0 " href={res.appyLink}>
-                  
-                  <span style={{ color: "blue", overflow: "hidden",  marginTop:'-10px'}}>
+              <div style={{ display: "flex" }} key={index}>
+                <a style={{ padding: "2rem" }} className="mt-0 " href={res.appyLink}>
+                  <span style={{ color: "blue", overflow: "hidden", marginTop: "-10px" }}>
                     {res.postTitle}
                   </span>{" "}
-                  <hr></hr>
+                  <hr />
                 </a>
-              </div >
+              </div>
             );
           })}
         </div>
